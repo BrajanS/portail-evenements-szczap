6e6debdad9da98e6563eeb8cb65854334e067667
BrajanS <szczapbrajan33@gmail.com>
2025-05-23 12:09:15 +0200

    feat: Boutton Ajouter/Retirer gerer par localStorage partie 1

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ themeBtn.addEventListener("click", () => {
   themeBtn.textContent = current === "claire" ? "Claire" : "Sombre";
   document.body.setAttribute("data-theme", next);
 });
-
+// #region Constantes Globales
 const sectionEvents = document.getElementById("sectionEvents");
+const sectionMonPlanning = document.getElementById("sectionMonPlanning");
 const templateEvents = document.getElementById("templateEvents");
+const templateDetails = document.getElementById("templateDetails");
 const listEvents = document.getElementById("listEvents");
 const listFavoris = document.getElementById("listFavoris");
-const templateDetails = document.getElementById("templateDetails");
-
 const baseUrl =
   "https://demo.theeventscalendar.com/wp-json/tribe/events/v1/events";
+// #endregion
 
 async function getData(url) {
   try {
@@ -27,22 +28,37 @@ async function getData(url) {
   }
 }
 
-async function listGenerate() {
+async function listEventsGenerate() {
   const event = await getData(baseUrl);
-  event.forEach((eventObject) => {
+  const storedEvents = JSON.parse(localStorage.getItem("eventsFavoris")) || [];
+  console.log("storedEvents", storedEvents);
+  // Clone seulement les données de "event" qui n'éxiste pas déjà au LocalStorage
+  const verifiedEvents = event.filter((data) =>
+    storedEvents.every((storedData) => storedData.id !== data.id)
+  );
+  console.log("verifiedEvents", verifiedEvents);
+  // Appel de la fonction qui affiche les données stoquer au LocalStorage
+  listFavorisGenerate(storedEvents);
+
+  verifiedEvents.forEach((eventObject) => {
+    // #region Template de "event" et les Selecteurs
     const model = templateEvents.content.cloneNode(true);
     const title = model.querySelector("h3");
     const date = model.querySelector("div > div > p:first-child > span");
     const lieu = model.querySelector("div > div > p:nth-child(2)> span");
-    const detailsBtn = model.querySelector("div > div > button");
+    const lesBoutons = model.querySelectorAll("button");
+    const detailsBtn = lesBoutons[0];
+    const ajouterBtn = lesBoutons[1];
+    // #endregion
 
     title.textContent = eventObject.title;
     date.textContent = eventObject.date;
+    lieu.textContent = !Array.isArray(eventObject.venue)
+      ? `${eventObject.venue.address} | ${eventObject.venue.city}`
+      : "Inconnu";
     detailsBtn.addEventListener("click", () => {
       const lookForDetails = document.querySelector("#details");
       if (!lookForDetails) {
-        console.log(lookForDetails);
-
         details(
           eventObject.start_date,
           eventObject.end_date,
@@ -51,8 +67,6 @@ async function listGenerate() {
           sectionEvents
         );
       } else {
-        console.log(lookForDetails);
-
         lookForDetails.remove();
         details(
           eventObject.start_date,
@@ -64,15 +78,77 @@ async function listGenerate() {
       }
     });
 
-    lieu.textContent = !Array.isArray(eventObject.venue)
-      ? `${eventObject.venue.address} | ${eventObject.venue.city}`
-      : "Inconnu";
+    ajouterBtn.addEventListener("click", () => {
+      const getFavoris =
+        JSON.parse(localStorage.getItem("eventsFavoris")) || [];
+      getFavoris.push(eventObject);
+      localStorage.setItem("eventsFavoris", JSON.stringify(getFavoris));
+      ajouterBtn.parentElement.parentElement.remove();
+    });
 
     listEvents.appendChild(model);
   });
 }
 
-listGenerate();
+function listFavorisGenerate(favoritesData) {
+  // Vide la liste de favoris (car cette fonction sera appeler aussi à chaque cliques "Ajouter")
+  listFavoris.replaceChildren();
+  favoritesData.forEach((favData) => {
+    // #region Template de "event" et les Selecteurs
+    const model = templateEvents.content.cloneNode(true);
+    const title = model.querySelector("h3");
+    const date = model.querySelector("div > div > p:first-child > span");
+    const lieu = model.querySelector("div > div > p:nth-child(2)> span");
+    const lesBoutons = model.querySelectorAll("button");
+    const detailsBtn = lesBoutons[0];
+    const retirerBtn = lesBoutons[1];
+    // #endregion
+
+    title.textContent = favData.title;
+    date.textContent = favData.date;
+    lieu.textContent = !Array.isArray(favData.venue)
+      ? `${favData.venue.address} | ${favData.venue.city}`
+      : "Inconnu";
+    detailsBtn.addEventListener("click", () => {
+      const lookForDetails = document.querySelector("#details");
+      if (!lookForDetails) {
+        details(
+          favData.start_date,
+          favData.end_date,
+          favData.description,
+          favData.url,
+          sectionMonPlanning
+        );
+      } else {
+        lookForDetails.remove();
+        details(
+          favData.start_date,
+          favData.end_date,
+          favData.description,
+          favData.url,
+          sectionMonPlanning
+        );
+      }
+    });
+    retirerBtn.textContent = "Retirer";
+    retirerBtn.addEventListener("click", () => {
+      const getFavoris =
+        JSON.parse(localStorage.getItem("eventsFavoris")) || [];
+      const removedFavoris = getFavoris.filter(
+        (deleteIfFound) => deleteIfFound.id !== favData.id
+      );
+      console.log("Verif suppr", removedFavoris);
+
+      // localStorage.setItem("eventsFavoris", JSON.stringify(getFavoris));
+      // ajouterBtn.parentElement.parentElement.remove();
+    });
+
+    listFavoris.appendChild(model);
+  });
+}
+
+// localStorage.clear();
+listEventsGenerate();
 
 function details(dateStart, dateEnd, desc, extLink, insertInto) {
   const modelDetails = templateDetails.content.cloneNode(true);
